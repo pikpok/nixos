5ac069b47675ca5fbe11d7e9cd0746ea2b1fadc0
Krzysztof Bezrąk <krzysztof@bezrak.pl>
2021-12-06 00:04:36 +0100

    firefox: disable built-in password manager

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,7 +1,12 @@
 { pkgs, home-manager, ... }: {
   home-manager.users.pikpok.programs.firefox = {
     enable = true;
-    profiles.default = { id = 0; };
+    profiles.default = {
+      id = 0;
+      settings = {
+        "signon.rememberSignons" = false;
+      };
+    };
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
       ublock-origin
       keepassxc-browser
