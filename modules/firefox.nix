7f66e394174abea26422f51da7a087d8d87099a0
Krzysztof Bezrąk <krzysztof@bezrak.pl>
2021-12-09 00:11:53 +0100

    add fx_cast

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,4 +1,9 @@
-{ pkgs, home-manager, ... }: {
+{ pkgs, home-manager, lib, ... }:
+let
+  # fx_cast doesn't doesn't work on aarch64 yet
+  includeFxCast = pkgs.system == "x86_64-linux";
+in
+{
   home-manager.users.pikpok.programs.firefox = {
     enable = true;
     profiles.default = {
@@ -11,6 +16,11 @@
       ublock-origin
       keepassxc-browser
       vimium
+      (lib.mkIf includeFxCast pkgs.nur.repos.ijohanne.firefoxPlugins.fx-cast)
     ];
   };
+
+  environment.systemPackages = [
+    (lib.mkIf includeFxCast pkgs.fx_cast_bridge)
+  ];
 }
