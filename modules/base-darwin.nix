3b10e9579105cbe8ca8a5687d0f5feec01989905
Krzysztof Bezrąk <krzysztof@bezrak.pl>
2024-04-26 11:14:21 +0200

    update flakes, add more resources to the darwin linux-builder

--- modules/base-darwin.nix
+++ modules/base-darwin.nix
@@ -3,7 +3,20 @@
 
   nix = {
     useDaemon = true;
-    linux-builder.enable = true;
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 40 * 1024;
+            memorySize = 8 * 1024;
+          };
+          cores = 6;
+        };
+      };
+    };
   };
 
   fonts.fontDir.enable = true;
