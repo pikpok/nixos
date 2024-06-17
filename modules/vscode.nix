f10d0edfc97107bf33f75c3e73b8fab850e1bb09
Krzysztof Bezrąk <krzysztof@bezrak.pl>
2024-06-17 21:46:12 +0200

    fix font configuration on darwin

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,15 +1,8 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [vscode];
 
-  fonts = let
-    fnts = with pkgs; [fira-code];
-  in
-    # TODO: hack to get around waiting for nix-darwin#870
-    ({fontDir.enable = true;}
-      // lib.mkIf pkgs.stdenv.isLinux {packages = fnts;}
-      // lib.mkIf pkgs.stdenv.isDarwin {fonts = fnts;});
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [fira-code];
+  };
 }
