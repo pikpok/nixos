59a09e39dd087a3138718f45b0046216e3f5bdfc
Krzysztof Bezrąk <krzysztof@bezrak.pl>
2025-01-06 16:03:46 +0100

    domino: enable experimental Bluetooth settings for home-assistant bluetooth support

--- hosts/domino/hardware-configuration.nix
+++ hosts/domino/hardware-configuration.nix
@@ -51,5 +51,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings.General.Experimental = true;
+
   powerManagement.powertop.enable = true;
 }
