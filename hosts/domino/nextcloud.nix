75788c9c83adeaf5b891f8a15d219d95585be457
Krzysztof Bezrąk <krzysztof@bezrak.pl>
2026-04-02 23:59:31 +0200

    Upgrade Nextcloud to 33

--- hosts/domino/nextcloud.nix
+++ hosts/domino/nextcloud.nix
@@ -11,7 +11,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud32;
+    package = pkgs.nextcloud33;
     notify_push.enable = true;
     maxUploadSize = "10G";
     hostName = "c.pikpok.xyz";
